cc9fba46cacf2253f9f288b7bc5f02067ec06c30
Tanguy De Brabandere <125645329+TanguyDeBrabandere@users.noreply.github.com>
2025-01-23 19:54:05 +0100

    Update settings.py

--- trade_journal/trade_journal/settings.py
+++ trade_journal/trade_journal/settings.py
@@ -59,7 +59,7 @@ DEFAULT_FILE_STORAGE = 'users.storage_backend.MediaStorage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # s3 static settings
-STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/static/'
+STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/'
 MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/media/'
 
 
